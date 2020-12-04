920fa2d03e16c1faeb61658cb891f238bdc8059f
Emil <emil_johansson92@hotmail.se>
2020-12-04 07:22:22 +0100

    Klar med dagen

--- index.js
+++ index.js
@@ -29,19 +29,90 @@ function getPassports(content) {
 function countValidPassports(passports) {
     let count = 0;
     passports.forEach(passport => {
-        console.log(passport);
         valid = true;
+        console.log(passport);
         reqFields.forEach(req => {
             if (valid) {
                 valid = passport.hasOwnProperty(req);
             }
         });
-        console.log("Valid=", valid);
+        if (valid) {
+            console.log(valid)
+            valid = validatePassport(passport);
+            console.log(valid)
+        }
         count = valid ? count + 1 : count;
     });
     return count;
 }
 
+function validatePassport(passport) {
+    valid = true;
+
+    if (valid) {
+        valid = validateNumber(passport.byr, 1920, 2002);
+        console.log("byr=", valid)
+    }
+    if (valid) {
+        valid = validateNumber(passport.iyr, 2010, 2020);
+        console.log("iyr=", valid)
+    }
+    if (valid) {
+        valid = validateNumber(passport.eyr, 2020, 2030);
+        console.log("eyr=", valid)
+    }
+    if (valid) {
+        valid = validateHeight(passport.hgt);
+        console.log("hgt=", valid)
+    }
+    if (valid) {
+        valid = validateHairColour(passport.hcl);
+        console.log("hcl=", valid)
+    }
+    if (valid) {
+        valid = validateEyeColour(passport.ecl);
+        console.log("ecl=", valid)
+    }
+    if (valid) {
+        valid = validatePID(passport.pid);
+        console.log("pid=", valid)
+    }
+    
+    return valid;
+}
+
+function validateNumber(n, _min, _max) {
+    return _min <= n && n <= _max;
+}
+
+function validateHeight(h) {
+    let valid = true;
+    const pMetric = h.slice(-2)
+    if (pMetric === 'cm') {
+        valid = validateNumber(parseInt(h.slice(0, h.length - 2)), 150, 193)
+    } else if (pMetric === 'in') {
+        valid = validateNumber(parseInt(h.slice(0, h.length - 2)), 59, 76)
+    } else {
+        valid = false;
+    }
+    return valid;
+}
+
+function validateHairColour(h) {
+    const re = /^\#[a-f0-9]{6}$/
+    return re.test(h);
+}
+
+function validateEyeColour(e) {
+    return ['amb','blu','brn','gry','grn','hzl','oth'].includes(e);
+}
+
+function validatePID(p) {
+    const re = /^\d{9}$/
+    return re.test(p);
+}
+
 const reqFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', /* 'cid' */];
+
 
 console.log(countValidPassports(resolveInput('input.txt')));
